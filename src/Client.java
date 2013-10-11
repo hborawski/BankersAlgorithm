43aa8334a6e83d074fabc29ae261dd3ea539f526
Harris Borawski <harrisborawski@me.com>
2013-10-11 13:09:36 -0400

    more method stubs

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,10 @@
 
-public class Client {
-
+public class Client extends Thread{
+	public Client(String name, Banker banker, int nUnits, int nRequests, long minSleepMillis, long maxSleepMillis){
+		
+	}
+	
+	public void run(){
+		
+	}
 }
